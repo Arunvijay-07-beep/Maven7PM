fb046f436f9ec08a2f7e10e6bc46fb7902dc5215
Arunvijay-07-beep <govindarajmeena43@gmail.com>
2025-07-28 15:07:33 +0530

    First Time Push

---
+++ src/test/java/org/tcs/Sample.java
@@ -0,0 +1,14 @@
+package org.tcs;
+
+public class Sample {
+
+	private void method1() {
+		System.out.println("Method 1");
+	}
+	private void method2() {
+		System.out.println("Method 2");
+	}
+	private void method3() {
+		System.out.println("Method 3");
+	}
+}
