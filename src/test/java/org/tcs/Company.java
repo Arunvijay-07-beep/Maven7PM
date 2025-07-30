a5e336387259f57ccda11d333cc7049ab978c832
Arunvijay-07-beep <govindarajmeena43@gmail.com>
2025-07-30 21:18:45 +0530

    Added One More  Method in Company Class

--- src/test/java/org/tcs/Company.java
+++ src/test/java/org/tcs/Company.java
@@ -2,4 +2,16 @@ package org.tcs;
 
 public class Company {
 
+	
+	private void javaa() {
+System.out.println("JAVA");
+	}
+	private void pythonnn() {
+System.out.println("PYTHON");
+	}
+	private void javaaaaa() {
+		// TODO Auto-generated method stub
+
+	}
+	
 }
